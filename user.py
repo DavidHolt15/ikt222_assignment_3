eeb7e66ecacd7873866ec9fd36df654b74ac5e2f
Davidbh <davidbh@uia.no>
2024-11-12 14:17:19 +0100

    Added encryption to secure_key

--- user.py
+++ user.py
@@ -7,6 +7,15 @@ import base64
 from datetime import datetime, timedelta
 from io import BytesIO
 from flask import flash
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+from flask.cli import load_dotenv
+
+load_dotenv()
+
+encryption_key_base64 = os.getenv('ENCRYPTION_KEY')
+ENCRYPTION_KEY = base64.b64decode(encryption_key_base64)
 
 # Database connection utilities
 def get_db_connection(db_name='database.db'):
@@ -17,14 +26,32 @@ def get_db_connection(db_name='database.db'):
 # Password hashing and verification
 def hash_password(password):
     salt = os.urandom(16)
-    password_hash = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+    password_hash = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 250000)
     return salt + password_hash
 
 def verify_password(stored_password, provided_password):
     salt = stored_password[:16]
     stored_password_hash = stored_password[16:]
-    provided_password_hash = hashlib.pbkdf2_hmac('sha256', provided_password.encode('utf-8'), salt, 100000)
+    provided_password_hash = hashlib.pbkdf2_hmac('sha256', provided_password.encode('utf-8'), salt, 250000)
     return provided_password_hash == stored_password_hash
+
+def encrypt_secret(secret):
+    iv = os.urandom(16)
+    cipher = Cipher(algorithms.AES(ENCRYPTION_KEY), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    padder = padding.PKCS7(128).padder()
+    padded_secret = padder.update(secret.encode()) + padder.finalize()
+    encrypted_secret = encryptor.update(padded_secret) + encryptor.finalize()
+    return iv + encrypted_secret
+
+def decrypt_secret(encrypted_secret):
+    iv = encrypted_secret[:16]
+    cipher = Cipher(algorithms.AES(ENCRYPTION_KEY), modes.CBC(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    decrypted_padded_secret = decryptor.update(encrypted_secret[16:]) + decryptor.finalize()
+    unpadder = padding.PKCS7(128).unpadder()
+    secret = unpadder.update(decrypted_padded_secret) + unpadder.finalize()
+    return secret.decode()
 
 # User registration with TOTP setup
 def register_user(email, password, confirm_password):
@@ -34,23 +61,23 @@ def register_user(email, password, confirm_password):
 
     password_hash = hash_password(password)
     totp_secret = pyotp.random_base32()
+    encrypted_secret = encrypt_secret(totp_secret)
 
     with get_db_connection() as conn:
         try:
-            conn.execute(
-                '''
-                INSERT INTO users (email, password_hash, secret_key, failed_attempts)
-                VALUES (?, ?, ?, ?)
-                ''', (email, password_hash, totp_secret, 0)
-            )
-            conn.commit()
-
             # Generate TOTP QR code
             totp_uri = pyotp.totp.TOTP(totp_secret).provisioning_uri(name=email, issuer_name="ReviewMannen")
             qr = qrcode.make(totp_uri)
             buf = BytesIO()
             qr.save(buf)
             qr_base64 = base64.b64encode(buf.getvalue()).decode('utf-8')
+            conn.execute(
+                '''
+                INSERT INTO users (email, password_hash, secret_key, failed_attempts)
+                VALUES (?, ?, ?, ?)
+                ''', (email, password_hash, encrypted_secret, 0)
+            )
+            conn.commit()
             return qr_base64, True
         except sqlite3.IntegrityError:
             flash("Email already exists", "danger")
@@ -93,7 +120,8 @@ def login_user(email, password, totp_code):
     if user and user['password_hash']:
         if verify_password(user['password_hash'], password):
             # Verify TOTP code if the password is correct
-            totp = pyotp.TOTP(user['secret_key'])
+            decrypted_secret = decrypt_secret(user['secret_key'])
+            totp = pyotp.TOTP(decrypted_secret)
             if totp.verify(totp_code):
                 # Reset failed attempts on successful login
                 with get_db_connection() as conn:
